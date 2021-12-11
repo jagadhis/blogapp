b3ba5f7f5a0c6a9c346bdeb9bd9609438fa07d91
jagadhis <jwaran78@gmail.com>
2021-12-11 15:05:13 +0530

    commit

--- Services/Index.js
+++ Services/Index.js
@@ -35,3 +35,44 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
   s;
 };
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query getPostDetails(){
+        posts(orderBy:createdAt_ASC
+            last:3){
+                title
+                featuredImage{
+                    url
+                }
+                createdAt
+                slug
+            }
+    }
+    `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const gerSimilarPosts = async () => {
+  const query = gql`
+    query getPostDetails($slug: string!, $categories: [string!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
